4ac417d42144072f1a5d500165641e947a33a3e3
abhaysinghtomar97 <abhaysinghtomar97@gmail.com>
2025-08-23 20:13:38 +0530

    register, login, logout, templates, user authentication

--- FSP1/tweet/forms.py
+++ FSP1/tweet/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 # from django.shortcuts import rens
 from .models import Tweet
-
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text', 'photo']
         # return render(request, 'tweet_form.html')
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        
